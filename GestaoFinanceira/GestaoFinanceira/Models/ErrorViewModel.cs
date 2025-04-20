4b066213b4db63445a2cc3526d89c08b51a1520c
João Bosco <ti.junior@gmail.com>
2025-04-20 15:26:06 -0300

    Start project

---
+++ GestaoFinanceira/GestaoFinanceira/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoFinanceira.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
